6f82920adb6ed26943ae9d54b1fe579339c7e093
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-01-21 16:00:08 +0530

    Drilled props deep into the component tree

--- src/App.js
+++ src/App.js
@@ -51,20 +51,21 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar />
-      <Main />
+      <NavBar movies={movies}/>
+      <Main movies={movies}/>
     </>
   );
 }
 
-function NavBar() {
+function NavBar({ movies }) {
   return (
     <nav className="nav-bar">
       <Logo />
       <Search />
-      <NumResults />
+      <NumResults movies={movies}/>
     </nav>
   );
 }
@@ -91,24 +92,24 @@ function Search() {
   );
 }
 
-function NumResults() {
+function NumResults({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>X</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   );
 }
 
-function Main() {
+function Main({ movies }) {
   return (
     <main className="main">
-      <ListBox />
+      <ListBox movies={movies}/>
       <WatchedBox />
     </main>
   );
 }
 
-function ListBox() {
+function ListBox({ movies }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -119,13 +120,13 @@ function ListBox() {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList />}
+      {isOpen1 && <MovieList movies={movies}/>}
     </div>
   );
 }
 
-function MovieList() {
-  const [movies, setMovies] = useState(tempMovieData);
+function MovieList({ movies }) {
+  
   return (
     <ul className="list">
       {movies?.map((movie) => (
@@ -152,7 +153,6 @@ function Movie({ movie }) {
 
 function WatchedBox() {
   const [watched, setWatched] = useState(tempWatchedData);
-
   const [isOpen2, setIsOpen2] = useState(true);
 
 
@@ -209,10 +209,10 @@ function WatchedSummary({ watched }){
 function WatchedMoviesList({ watched }){
   return (
     <ul className="list">
-            {watched.map((movie) => (
-              <MovieList movie={movie} key={movie.imdbID}/>
-            ))}
-          </ul>
+        {watched.map((movie) => (
+          <MovieList movie={movie} key={movie.imdbID}/>
+        ))}
+    </ul>
   )
 }
 
